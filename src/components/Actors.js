8d00ad36df0098a370541788590faeaf340316d8
cynthia <cchelangat9892gmail.com>
2022-10-01 09:01:51 +0300

    Completed Assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsLIst = actors.map((actor, index) => {
+    return (
+      <div key={index}>
+        <h2>Name: {actor.name}</h2>
+        <h4>
+          Movies:{" "}
+          <ul>
+            {actor.movies.map((movie, index) => {
+              return <li key={index}>{movie}</li>;
+            })}
+          </ul>
+        </h4>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsLIst}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,70 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
-}
+
+
+  const linkStyles = {
+    display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "Red",
+  textDecoration: "none",
+  color: "white",
+  };
+
+  function NavBar() {
+    return (
+      <div className="navbar">
+        <NavLink 
+        to="/"
+        exact 
+        style={linkStyles}
+        activeStyle={{
+          background:"blue",
+        }}
+        >
+          Home
+          </NavLink>
+
+          <NavLink 
+        to="/movies"
+        exact 
+        style={linkStyles}
+        activeStyle={{
+          background:"blue",
+        }}
+        >
+          Movies
+          </NavLink>
+
+          <NavLink 
+        to="/directors"
+        exact 
+        style={linkStyles}
+        activeStyle={{
+          background:"darkgreen",
+        }}
+        >
+          Directors
+          </NavLink>
+
+          <NavLink 
+        to="/actors"
+        exact 
+        style={linkStyles}
+        activeStyle={{
+          background:"darkgreen",
+        }}
+        >
+          Actors
+          </NavLink>
+
+
+
+      </div>
+    );
+  }
+
 
 export default NavBar;
